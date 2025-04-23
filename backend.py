f0bcf9f4b3d2abec449dd3f8da6cb8dc714214d0
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-04-23 23:28:11 +0330

    Done

--- backend.py
+++ backend.py
@@ -3,12 +3,18 @@ import requests
 API_KEY = "4fe5208a9b83a94d04d7f9ee1a29c2a7"
 PI = "141710af2113bab9f55ef73e1bcd33d5"
 
-def get_data(place, forecast_days=None, k_type=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={PI}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data['list']
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data("Tokyo"))
+    try:
+        print(get_data("Tokyo", forecast_days=3))
+    except:
+        print("City not found.")
--- main.py
+++ main.py
@@ -3,15 +3,40 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather Forecast for Next Days")
-place = st.text_input("Place: ")
+place = st.text_input("City: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                 help="Select the number of forecated days")
 option = st.selectbox("Select data to view",
-                    ("Temperture", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+                    ("Temperature", "Sky"))
 
-data = get_data(place, days, option)
+if place:
+    try:
+        filtered_data = get_data(place, days)
+        st.subheader(f"{option} for the next {days} days in {place}")
+        # Get data
 
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create Temperature plot
+            figure = px.line(x=dates, y=temperature,
+                    labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {
+                    "Clear": "images/clear.png",
+                    "Clouds": "images/cloud.png",
+                    "Rain": "images/rain.png",
+                    "Snow": "images/snow.png"
+                    }
+            sky = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky]
+            # Create Sky Plot
+            st.image(image_paths, width=85)
+    except KeyError:
+        st.subheader(f"Is {place} a real place? ")
+
+else:
+    st.subheader("First, type a city name!")
